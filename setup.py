cf83601f97f589b53047323a80d45acb11aa3d95
ZSendokame <sendo@sendokame.com>
2023-10-01 21:07:42 -0300

    Metadata.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+long_description = open('./README.md')
+
+setup(
+    name='Penv',
+    version='1.0.0',
+    url='https://github.com/ZSendokame/Penv',
+    license='MIT license',
+    author='ZSendokame',
+    description='.env parser.',
+    long_description=long_description.read(),
+    long_description_content_type='text/markdown',
+
+    packages=(find_packages(include=['penv']))
+)
